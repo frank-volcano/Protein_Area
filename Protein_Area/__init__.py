eb2ed27a59af664bed9c8dd7462c864bc326f5d0
ChenouZhang <czhan178@asu.edu>
2024-06-25 16:06:49 -0700

    updated env dependency files

--- Protein_Area/__init__.py
+++ Protein_Area/__init__.py
@@ -5,5 +5,6 @@ A python package to calculate protein cross-section area from MD simulations.
 
 # Add imports here
 from importlib.metadata import version
+from .ProteinArea import ProteinArea
 
 __version__ = version("Protein_Area")
