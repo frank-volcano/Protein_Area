b6d87a7fb720ed7c755c7eb20bfd81059677ceee
Chenou Zhang <czhan178@asu.edu>
2024-06-25 14:38:59 -0700

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ Protein_Area/__init__.py
@@ -0,0 +1,9 @@
+"""
+Protein_Area
+A python package to calculate protein cross-section area from MD simulations.
+"""
+
+# Add imports here
+from importlib.metadata import version
+
+__version__ = version("Protein_Area")
