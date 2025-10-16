48087d257d5f07f1d9fd479676e68b885fc7a418
Marshal <marshalo.loo@colorado.edu>
2025-10-15 19:54:22 -0500

    trial 2 update

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Protein_Area
+A python package to calculate protein cross-section area from MD simulations.
+"""
+
+# Add imports here
+from importlib.metadata import version
+from .ProteinArea import ProteinArea
+
+__version__ = version("Protein_Area")
